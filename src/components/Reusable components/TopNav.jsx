b1957cd72aafd7a32438528a2a28b8b5f44b8c05
Alessia <98785381+amorea9@users.noreply.github.com>
2023-01-16 12:38:11 +0100

    opmitising logo to webp

--- src/components/Reusable components/TopNav.jsx
+++ src/components/Reusable components/TopNav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import logo from "../../media/foofest-logo.png";
+import logo from "../../media/foofest-logo.webp";
 
 function TopNav(props) {
   const navigate = useNavigate();
